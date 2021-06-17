886276c68bad7daee81b1354fdb3486b8838b811
Alex G <hi@mavenblueprint.com>
2021-06-17 09:02:46 -0400

    Improve transporter to avoid breaking when SSH drops out preventing drive 3 and 4 from never starting

--- scripts/transporter.sh
+++ scripts/transporter.sh
@@ -2,13 +2,13 @@
 # Since the 4 Mac mini M1 machines are plotting to /Volumes/chiaplot-ext-# and I don't want to mount any extra USB drives in this case I manually move plots from the /Volumes/chiaplot-ext-# to the other USB drives only mounted on Mac mini M1 #1 machine
 # Send script to Mac mini #1:
 # scp scripts/transporter.sh adminofthemac1@192.168.1.211:/Users/adminofthemac1/Documents/
-# Start transporter in the background:
-# sh -c './transporter.sh' & jobs
+# SCREEN -d -m -S sas1 bash -c '~/Documents/transporter2.sh';screen -x sas1
 
-sleep 0;    SRC_DRIVE="/Volumes/chiaplot-ext-1";DEST_DRIVE="/Volumes/chia-xt-2-3";SCREEN_NAME="tr1"; pushd ${SRC_DRIVE};ls -Al plot-*.plot | tr -s ' ' | cut -f9- -d' ' | head -5 > ${SRC_DRIVE}/_transporter_list.txt;popd;cat ${SRC_DRIVE}/_transporter_list.txt;SCREEN -d -m -S $SCREEN_NAME bash -c 'rsync -aP --remove-source-files --files-from='${SRC_DRIVE}'/_transporter_list.txt '${SRC_DRIVE}' '${DEST_DRIVE}''; screen -ls
+# adding sleep just in case there's other network IO
+NUM_PLOTS_TO_MOVE=4;
+sleep 2;SRC_DRIVE="/Volumes/chiaplot-ext-1";DEST_DRIVE="/Volumes/chiasas1";pushd ${SRC_DRIVE};ls -Al plot-*.plot | tr -s ' ' | cut -f9- -d' ' | head -${NUM_PLOTS_TO_MOVE} > ${SRC_DRIVE}/_transporter_list.txt;popd;cat ${SRC_DRIVE}/_transporter_list.txt;rsync -aP --remove-source-files --files-from=${SRC_DRIVE}/_transporter_list.txt ${SRC_DRIVE} ${DEST_DRIVE};
+echo "sleep";sleep 900;SRC_DRIVE="/Volumes/chiaplot-ext-2";DEST_DRIVE="/Volumes/chiasas1";pushd ${SRC_DRIVE};ls -Al plot-*.plot | tr -s ' ' | cut -f9- -d' ' | head -${NUM_PLOTS_TO_MOVE} > ${SRC_DRIVE}/_transporter_list.txt;popd;cat ${SRC_DRIVE}/_transporter_list.txt;rsync -aP --remove-source-files --files-from=${SRC_DRIVE}/_transporter_list.txt ${SRC_DRIVE} ${DEST_DRIVE};
+echo "sleep";sleep 900;SRC_DRIVE="/Volumes/chiaplot-ext-3";DEST_DRIVE="/Volumes/chiasas1";pushd ${SRC_DRIVE};ls -Al plot-*.plot | tr -s ' ' | cut -f9- -d' ' | head -${NUM_PLOTS_TO_MOVE} > ${SRC_DRIVE}/_transporter_list.txt;popd;cat ${SRC_DRIVE}/_transporter_list.txt;rsync -aP --remove-source-files --files-from=${SRC_DRIVE}/_transporter_list.txt ${SRC_DRIVE} ${DEST_DRIVE};
+echo "sleep";sleep 900;SRC_DRIVE="/Volumes/chiaplot-ext-4";DEST_DRIVE="/Volumes/chiasas1";pushd ${SRC_DRIVE};ls -Al plot-*.plot | tr -s ' ' | cut -f9- -d' ' | head -${NUM_PLOTS_TO_MOVE} > ${SRC_DRIVE}/_transporter_list.txt;popd;cat ${SRC_DRIVE}/_transporter_list.txt;rsync -aP --remove-source-files --files-from=${SRC_DRIVE}/_transporter_list.txt ${SRC_DRIVE} ${DEST_DRIVE};
 
-sleep 3600; SRC_DRIVE="/Volumes/chiaplot-ext-2";DEST_DRIVE="/Volumes/chia-xt-2-3";SCREEN_NAME="tr2"; pushd ${SRC_DRIVE};ls -Al plot-*.plot | tr -s ' ' | cut -f9- -d' ' | head -5 > ${SRC_DRIVE}/_transporter_list.txt;popd;cat ${SRC_DRIVE}/_transporter_list.txt;SCREEN -d -m -S $SCREEN_NAME bash -c 'rsync -aP --remove-source-files --files-from='${SRC_DRIVE}'/_transporter_list.txt '${SRC_DRIVE}' '${DEST_DRIVE}''; screen -ls
 
-sleep 3600; SRC_DRIVE="/Volumes/chiaplot-ext-3";DEST_DRIVE="/Volumes/chia-xt-2-3";SCREEN_NAME="tr3"; pushd ${SRC_DRIVE};ls -Al plot-*.plot | tr -s ' ' | cut -f9- -d' ' | head -5 > ${SRC_DRIVE}/_transporter_list.txt;popd;cat ${SRC_DRIVE}/_transporter_list.txt;SCREEN -d -m -S $SCREEN_NAME bash -c 'rsync -aP --remove-source-files --files-from='${SRC_DRIVE}'/_transporter_list.txt '${SRC_DRIVE}' '${DEST_DRIVE}''; screen -ls
-
-sleep 3600; SRC_DRIVE="/Volumes/chiaplot-ext-4";DEST_DRIVE="/Volumes/chia-xt-2-3";SCREEN_NAME="tr4"; pushd ${SRC_DRIVE};ls -Al plot-*.plot | tr -s ' ' | cut -f9- -d' ' | head -5 > ${SRC_DRIVE}/_transporter_list.txt;popd;cat ${SRC_DRIVE}/_transporter_list.txt;SCREEN -d -m -S $SCREEN_NAME bash -c 'rsync -aP --remove-source-files --files-from='${SRC_DRIVE}'/_transporter_list.txt '${SRC_DRIVE}' '${DEST_DRIVE}''; screen -ls
