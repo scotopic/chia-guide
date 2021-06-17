a62eaad1d71c841b6206d4d25dee486dbda1a709
Alex G <hi@mavenblueprint.com>
2021-06-17 09:03:53 -0400

    Fix script start command

--- scripts/transporter.sh
+++ scripts/transporter.sh
@@ -2,7 +2,7 @@
 # Since the 4 Mac mini M1 machines are plotting to /Volumes/chiaplot-ext-# and I don't want to mount any extra USB drives in this case I manually move plots from the /Volumes/chiaplot-ext-# to the other USB drives only mounted on Mac mini M1 #1 machine
 # Send script to Mac mini #1:
 # scp scripts/transporter.sh adminofthemac1@192.168.1.211:/Users/adminofthemac1/Documents/
-# SCREEN -d -m -S sas1 bash -c '~/Documents/transporter2.sh';screen -x sas1
+# SCREEN -d -m -S sas1 bash -c '~/Documents/transporter.sh';screen -x sas1
 
 # adding sleep just in case there's other network IO
 NUM_PLOTS_TO_MOVE=4;
